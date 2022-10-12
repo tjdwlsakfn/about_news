cd73be1d41f31deb97e5fce47af0d5d32b3ebdd0
tjdwlsakfn <84087795+tjdwlsakfn@users.noreply.github.com>
2022-10-12 11:31:09 +0900

    Add files via upload

---
+++ about_news.php
@@ -0,0 +1,250 @@
+<?php include 'sub-header.php' ?>
+<link rel="stylesheet" href="./css/category.css">
+<link rel="stylesheet" href="./css/about_news.css">
+<script src="./js/jquery-ui.min.js"></script>
+<script src="./js/category.js"></script>
+<script src="./js/about-news-visual.js"></script>
+<script src="./js/about-news.js"></script>
+<section class="about_news_visual">
+  <ul class="img_container">
+    <li class="imgbox1"><img src="./img/news/model/model1.png" alt=""></li>
+    <li class="imgbox2"><img src="./img/news/model/model2.png" alt=""></li>
+    <li class="imgbox3"><img src="./img/news/model/model9.png" alt=""></li>
+    <li class="imgbox4"><img src="./img/news/model/model11.png" alt=""></li>
+    <li class="imgbox5"><img src="./img/news/model/model12.png" alt=""></li>
+    <li class="imgbox6"><img src="./img/news/model/model10.png" alt=""></li>
+  </ul><!-- img_contaienr -->
+
+  <ul class="title_container type1">
+    <li class="title1">
+      <h3>Latest News1</h3>
+    </li>
+    <li class="title2">
+      <h3>Latest News2</h3>
+    </li>
+    <li class="title3">
+      <h3>Latest News3</h3>
+    </li>
+    <li class="title4">
+      <h3>Latest News4</h3>
+    </li>
+    <li class="title5">
+      <h3>Latest News5</h3>
+    </li>
+    <li class="title6">
+      <h3>Latest News6</h3>
+    </li>
+  </ul><!-- title_container -->
+
+  <div class="digits_text_container">
+    <div class="digits_container type2">
+      <ul class="digits1">
+        <li>0</li>
+        <li>2</li>
+        <li>3</li>
+        <li>4</li>
+        <li>5</li>
+        <li>6</li>
+        <li>7</li>
+        <li>8</li>
+        <li>9</li>
+      </ul>
+      <ul class="digits2">
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+        <li>4</li>
+        <li>5</li>
+        <li>6</li>
+        <li>7</li>
+        <li>8</li>
+        <li>9</li>
+      </ul>
+    </div><!-- digits_container -->
+    <div class="text_container_wrap">
+      <ul class="text_container type2">
+        <li class="text1">
+        The signature ingredients that make the House of Dior unique in the world of luxury beauty are sourced from flowers. 
+        </li>
+        <li class="text2">
+        Our mission, to only leave beauty as a legacy, means being fully committed to reducing our carbon emissions by 46% full scope by 2030 and aligning our business with the 1.5°C pathway.
+        </li>
+        <li class="text3">
+        At the heart of everything we do lies the limitless love of flowers that inspired Monsieur Dior. 
+        </li>
+        <li class="text4">
+        To craft Parfums Christian Dior’s sustainability vision and strategy, we worked from the ground up by consulting a wide panel of internal
+        </li>
+        <li class="text5">
+        We commit to monitoring our progress as we move forward, aware that ambitious action is required on all fronts.
+        </li>
+        <li class="text6">
+        Parfums Christian Dior is particularly attentive to the local impact of its upstream supply chain.s
+        </li>
+      </ul>
+    </div><!-- text-container-wrap -->
+  </div>
+  <!--digitstext_container-->
+
+  <div class="indicator">
+    <button class="prev" data-dir="prev"><i class="far fa-arrow-alt-circle-left"></i></button>
+    <div class="progress">
+      <span></span>
+    </div>
+    <button class="next" data-dir="next"><i class="far fa-arrow-alt-circle-right"></i></button>
+    <p class="cnt type2"><sup>1</sup> / <sub>0</sub></p>
+  </div><!-- indicator -->
+</section>
+<!--section-about-news-1 ---------------------------------------------------------------------- -->
+<section class="section-about-news-1 section_category type4 light">
+  <div class="center_14">
+    <svg viewBox="0 0 941 936" preserveAspectRatio="xMidYMid slice">
+      <path class="path" pathLength="1" fill="none" stroke="#000000" stroke-miterlimit="10" d="M521.501,0c0,116.998-102,116.998-102,233.996
+           c0,116.997,102,116.997,102,233.994c0,117.002-102,117.002-102,234.005s102,117.003,102,234.005" />
+    </svg>
+    <h2 class="bold">2022 NEWS</h2>
+    <ul class="clearfix">
+      <li>
+        <div class="inner">
+          <figure>
+            <img src="./img/news/model/nmodel1.png" alt="">
+          </figure>
+          <div class="textbox">
+            <h3 class="bold">AUTUMN-WINTER 2022-2023 HAUTE COUTURE</h3>
+            <p>
+            The tree of life is at the heart of Ukrainian artist Olesia Trofymenko’s work, 
+            and the starting point for the Dior autumn-winter 2022-2023 haute couture collection designed by Maria Grazia Chiuri.
+            </p>
+            <a href="#">View More</a>
+          </div>
+        </div>
+      </li>
+      <li>
+        <div class="inner">
+          <figure>
+            <img src="./img/news/model/nmodel5.png" alt="">
+          </figure>
+          <div class="textbox">
+            <h3 class="bold">AUTUMN-WINTER 2022-2023 COLLECTION</h3>
+            <p>
+            Through the Dior autumn-winter 2022-2023 ready-to-wear collection, Maria Grazia Chiuri opens the doors to a new era.
+            he iconic Bar jacket is reinvented with technical inserts created with the Italian start-up D-Air lab*. 
+            From clothing to accessories, the creations interweave aesthetics and technology, transforming Dior heritage. 
+            A fascinating exploration of past, present and future.
+            </p>
+            <a href="#">View More</a>
+          </div>
+        </div>
+      </li>
+      <li>
+        <div class="inner">
+          <figure>
+            <img src="./img/news/model/model7.5.png" alt="">
+          </figure>
+          <div class="textbox">
+            <h3 class="bold">DIOR ESSENTIALS</h3>
+            <p>
+            Staying true to the history of Dior, the Dior Essentials collection defines a new menswear style to reflect the spirit of the House. 
+            As a visionary designer, Christian Dior captured a set of iconic codes at the heart of his creations.
+            Reinterpreted now, these are codes to embody the essence of his couturier.
+            </p>
+            <a href="#n">View More</a>
+          </div>
+        </div>
+      </li>
+      <li>
+        <div class="inner">
+          <figure>
+            <img src="./img/news/model/nmodel4.jpg" alt="">
+          </figure>
+          <div class="textbox">
+            <h3 class="bold">ROSE DES VENTS</h3>
+            <p>
+            Victoire de Castellane reimagines Christian Dior's lucky star in the form of a wind rose—a universal symbol of travel and adventure. 
+            This eight-pointed motif combines the couturier’s favorite number (eight), flower (a rose), and symbol (a star) into one timeless design.
+            </p>
+            <a href="#">View More</a>
+          </div>
+        </div>
+      </li>
+    </ul>
+  </div><!-- center14 -->
+</section>
+<!--section-about-news-2 ---------------------------------------------------------------------- -->
+<section class="section-about-news-2 section_category type3 dark">
+  <div class="center_14">
+    <svg viewBox="0 0 702 841" preserveAspectRatio="xMidYMid slice">
+      <path class="path" pathLength="1" fill="none" stroke="white" stroke-miterlimit="10" d="M351.5,66.5V95H242c-35.2,0-64,28.8-64,64v108h338 v103c0,35.2-28.8,64-64,64H242c-35.2,0-64,28.8-64,64v98h338c0,0,0,21.15,0,47s-28.8,47-64,47h-41c-35.2,0-64,28.8-64,64v87" />
+    </svg>
+    <h2 class="bold">2021 NEWS<</h2>
+
+    <ul class="clearfix">
+      <li>
+        <div class="inner">
+          <figure>
+            <img src="./img/news/news4.jpg" alt="">
+          </figure>
+          <div class="textbox">
+            <h3 class="bold">DIOR IN BLOOM</h3>
+            <p>
+            For the first time ever, the tales of Dior Perfumes are told in a Podcast series, 
+            Dior Untold. This series provides a unique opportunity to take us behind the scenes of the Dior Maison 
+            and its special savoir-faire. An immersion into the fascinating world of its creator and his innermost inspirations.
+            </p>
+            <a href="#">View More</a>
+          </div>
+        </div>
+      </li>
+      <li>
+        <div class="inner">
+          <figure>
+            <img src="./img/news/news3.jpg" alt="">
+          </figure>
+          <div class="textbox">
+            <h3 class="bold">AND WOMEN CREATED DIOR</h3>
+            <p>
+              After having transported you through senses, it is now through encounters 
+              that this second episode "And Women Created Dior" takes you on a journey through time.
+            </p>
+            <a href="#">View More</a>
+          </div>
+        </div>
+      </li>
+    </ul>
+
+    <ul class="clearfix">
+      <li>
+        <div class="inner">
+          <figure>
+            <img src="./img/news/news2.jpg" alt="">
+          </figure>
+          <div class="textbox">
+            <h3 class="bold">THE POWER OF RED</h3>
+            <p>
+            "The Power of Red" is the third episode of the Dior Untold saga, 
+            this time imbued with the revolutionary hues that have guided women towards their quest for empowerment.
+            </p>
+            <a href="#">View More</a>
+          </div>
+        </div>
+      </li>
+      <li>
+        <div class="inner">
+          <figure>
+            <img src="./img/news/news1.jpg" alt="">
+          </figure>
+          <div class="textbox">
+            <h3 class="bold">ROSE OF GRANVILLE</h3>
+            <p>
+              Flowers have been vital sources of inspiration for Dior’s legendary creations.
+            </p>
+            <a href="#">View More</a>
+          </div>
+        </div>
+      </li>
+
+    </ul>
+  </div><!-- center14 -->
+</section><!-- section-about-news-2 -->
+
+<?php include 'sub-footer.php' ?>
